1b05150d1bcf786436194e017844d747ffdb0cd4
Edward Neal <55035479+edwardneal@users.noreply.github.com>
2020-10-07 23:44:33 +0100

    Added an Azure Service Bus and corresponding queue in order to process email notifications, etc.

---
+++ Jibberwock.Core.Background/HandleNotifications.cs
@@ -0,0 +1,27 @@
+﻿using Jibberwock.Persistence.DataAccess.DataSources;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jibberwock.Core.Background
+{
+    public class HandleNotifications
+    {
+        private readonly SqlServerDataSource _dataSource;
+
+        public HandleNotifications(SqlServerDataSource dataSource) => _dataSource = dataSource;
+
+        [FunctionName("HandleNotifications")]
+        public async Task Run(
+            [ServiceBusTrigger("%CONNECTIONS_SERVICEBUS_QUEUES_NOTIFICATIONS%", Connection = "CONNECTIONS_SERVICEBUS_CONNECTIONSTRING")]
+            Message notificationMessage,
+            ILogger log)
+        {
+            log.LogInformation($"Received message on queue: {notificationMessage}");
+        }
+    }
+}
